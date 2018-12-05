e8c6e7d02b5e0a760ab369e7142c0669ebdd5fa6
S.Motoki <sam.moto.com@gmail.com>
2018-12-06 04:22:27 +0900

    いいね機能 実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
 
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
